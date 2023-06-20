e93fdfa8125c323204aad989fa769697feeecd72
Robert Gjorgjioski <robertgjo28@gmail.com>
2023-06-20 18:40:17 +0200

    Update PostStore

--- src/stores/postsStore.js
+++ src/stores/postsStore.js
@@ -7,29 +7,48 @@ export const usePostsStore = defineStore('posts', {
   }),
   getters: {
     getPostById: (state) => (id) => state.posts.find((post) => post.id === id),
-    getPosts() {
-      return this.posts;
-    },
   },
   actions: {
     async fetchPosts() {
-      const { data: users } = await api.get(endpoints.users);
-      const { data: posts } = await api.get(endpoints.posts);
-      const filteredPosts = posts.map((post) => {
-        const user = users.find((user) => user.id === post.userId);
-        return { ...post, user };
-      });
-      this.posts = filteredPosts;
+      try {
+        const { data: users } = await api.get(endpoints.users);
+        const { data: posts } = await api.get(endpoints.posts);
+
+        this.posts = posts.map((post) => {
+          const user = users.find((user) => user.id === post.userId);
+          return { ...post, user };
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
     async deletePost(id) {
-      await api.delete(endpoints.deletePost(id));
-      this.posts = this.posts.filter((post) => post.id !== id);
+      try {
+        await api.delete(endpoints.deletePost(id));
+        this.posts = this.posts.filter((post) => post.id !== id);
+      } catch (error) {
+        console.log(error);
+      }
     },
     async fetchComments(id) {
-      const { data } = await api.get(endpoints.getPostComments(id));
-      return data;
+      try {
+        const { data } = await api.get(endpoints.getPostComments(id));
+        return data;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
     },
     async updatePost(post) {
+      if (post.id > 100) {
+        const index = this.posts.findIndex((p) => p.id === post.id);
+        if (index !== -1) {
+          const user = this.posts[index].user;
+          this.posts[index] = { ...post, user };
+        }
+        return;
+      }
+
       const { data } = await api.put(endpoints.updatePost(post.id), post);
       const index = this.posts.findIndex((p) => p.id === post.id);
       if (index !== -1) {
@@ -38,29 +57,33 @@ export const usePostsStore = defineStore('posts', {
       }
     },
     async createPost(post) {
-      const { data } = await api.post(endpoints.posts, post);
-      const guestUser = {
-        id: 1,
-        name: 'Guest Guest',
-        username: 'Guest',
-        email: 'guest@guest.biz',
-        address: {
-          street: 'Kulas Light',
-          suite: 'Apt. 556',
-          city: 'Gwenborough',
-          zipcode: '92998-3874',
-          geo: { lat: '-37.3159', lng: '81.1496' },
-        },
-        phone: '1-770-736-8031 x56442',
-        website: 'guest.com',
-        company: {
-          name: 'Guest Company',
-          catchPhrase: 'We are guest company and we are here to help you!',
-          bs: 'How do Guest Company do it?',
-        },
-      };
+      try {
+        const { data } = await api.post(endpoints.posts, post);
+        const guestUser = {
+          id: 1,
+          name: 'Guest Guest',
+          username: 'Guest',
+          email: 'guest@guest.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: { lat: '-37.3159', lng: '81.1496' },
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'guest.com',
+          company: {
+            name: 'Guest Company',
+            catchPhrase: 'We are guest company and we are here to help you!',
+            bs: 'How do Guest Company do it?',
+          },
+        };
 
-      this.posts.unshift({ ...data, user: guestUser });
+        this.posts.unshift({ ...data, user: guestUser });
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 });
